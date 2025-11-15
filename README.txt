9da0f321666e3aebc07e5dd994532ec3ed44878c
Valoha90 <joaquin.rodriguez@est.fi.uncoma.edu.ar>
2025-11-15 23:30:04 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+
+#Solo es un docker para no andar webeando con las dependencias de netgui que son librerias de 32bits;
+#El paquete esta sacado de un repo en una pagiina de una universidad de españa pero no encuentro la pagina.
+#El paquete original fue creado por GSyC/MobiQuo group su pagina https://mobiquo.gsyc.urjc.es/netlab.html.
+#Y aca toy Joaquin Valentin Rodriguez Flores haciendo un docker para que los nuevos admin no instalen librerias de 32 bits; pdta este docker usa ubuntu de base para mayo compatibilidad (no recomendable para S.O. principal)
+#https://ubuntu.com/containers.
+#En sintesis esto esta pa usarse.
+#EL docker se debe ejecutal de la siguiente manera si no no funciona.
+
+docker run -it --rm     --network host  --tmpfs /dev/shm:rw,nosuid,nodev,exec,mode=0777      --ipc=host     --cap-add=ALL        -e DISPLAY=$DISPLAY     netgui-original:0.4.9 bash
+
+#Recomendacion ejecutar el script netgui_in_docker.sh para abrirlo y este script crea una carpeta en el home en el cual podes cargar los laboratorios para abrirlos dentro de netgui.
+
+#Tambien esto fue echo con la ayuda de Manuel Gomes Dasilva y Fransisco Nahuel Lista.
---
+++ netgui_in_docker.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+#Este es un script para inicio rapido ya que las opciones de docker son laaaargas
+#Crea un directorio donde podran poner los laboratios y cargarlos al docker al igual que podran editarlos
+DirNetguiLabs=$HOME/NetguiLabs
+mkdir $DirNetguiLabs 2> /dev/null
+#es una variable que guarda la id del docker
+IdDocker=52b42822ce5b8b1c32975768ae450048da3e0e7fd38cc071021b6501758d22ed
+#configura x11 para el apartado grafico 
+xhost +local:docker > /dev/null 2>&1
+#Esta linea de comando es necesaria para ejecutar el docker de netgui y que funcione todo es vital. 
+docker run -it --rm --network host --tmpfs /dev/shm:rw,nosuid,nodev,exec,mode=0777 --ipc=host -e DISPLAY=$DISPLAY  -v $DirNetguiLabs:/root  $IdDocker
+#-it   -i: Modo interactivo (mantiene STDIN abierto)   -t: Asigna una pseudo-terminal (TTY)
+#--network host Usa la stack de red del host directamente (sin aislamiento de red) el contenedor comparte la interfaz de red con el host pq netgui lo necesita(sin esto no ejecuta o puede que tenga limitaciones si lo restringes).
+#Combinados permiten la interacción con el contenedor como si fuera una terminal normal
+#( --rm ) Elimina automáticamente el contenedor cuando se detiene
+#( --network host )Usa la stack de red del host directamente (sin aislamiento de red) el contenedor comparte la interfaz de red con el host
+#( --tmpfs /dev/shm:rw,nosuid,nodev,exec,mode=0777 ) Monta un sistema de archivos temporal en /dev/shm
+#Opciones:
+#rw: Lectura/escritura
+#nosuid: No permite suid bits
+#nodev: No interpreta dispositivos especiales
+#exec: Permite ejecutar binarios
+#mode=0777: Permisos completos para todos los usuarios
+#--ipc=host
+#Comparte el espacio de nombres IPC (Inter-Process Communication) con el host
+#-e DISPLAY=$DISPLAY
+#Configura la variable de entorno DISPLAY para aplicaciones gráficas com tambien permite mostrar ventanas en el host
+#-v $DirNetguiLabs:/root
+#Monta el directorio local $DirNetguiLabs en /root del contenedor
+#$DirNetguiLabs es una variable de entorno
+#netgui-final:0.4.10
+#Es la imagen y versión que se ejecutará
